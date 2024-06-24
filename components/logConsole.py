b411300543506bd5b2de2530330d9e86b6e693d4
unknown <isaacvega361@gmail.com>
2024-06-24 11:27:15 -0400

    Se desabilita la consola de salida

--- components/logConsole.py
+++ components/logConsole.py
@@ -9,15 +9,17 @@ class LogConsole(ttk.Frame):
     self.create_widgets()
   
   def create_widgets(self):
-    self.console = ScrolledText(self, state="normal", font=("Consolas", 10), height=10, vbar=False, width=50)
+    self.console = ScrolledText(self, state="disabled", font=("Consolas", 10), height=10, vbar=False, width=50)
     self.console.pack(expand=True, fill="both")
     
   
   def write(self, text):
+    self.console.text.configure(state="normal")
     if len(self.console.text.get("1.0", END)) > 1:
       self.console.insert(END, "\n")
     self.console.insert(END, text)
     self.console.see(END)
+    self.console.text.configure(state="disabled")
     
   def clear(self):
     self.console.delete("1.0", END)
