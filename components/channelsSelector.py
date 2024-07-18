10e8467323ed23d399d1b345de9912dbef2922a3
unknown <isaacvega361@gmail.com>
2024-07-18 16:29:18 -0400

    Se cambia 273 por 272

--- components/channelsSelector.py
+++ components/channelsSelector.py
@@ -24,7 +24,7 @@ class ChannelSelector(ttk.Frame):
     self.max_entry = ttk.Entry(self, validate="focusout", validatecommand=(self.master.register(self.validate), '%P'))
     self.max_entry.pack(side=ttk.LEFT, padx=5, pady=5, fill=ttk.X, expand=True)
     self.min_entry.insert(0, 0)
-    self.max_entry.insert(0, 273)
+    self.max_entry.insert(0, 272)
     
   def validate(self, value):
     if value == "": return True
